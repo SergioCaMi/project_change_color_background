6b68da1fbd9d2fa7b3d0ee45fc2ab9a7da73352c
SergioCaMi <sergiocami84@gmail.com>
2025-03-25 11:19:13 +0100

    fin ejercicio

--- js/script.js
+++ js/script.js
@@ -1,4 +1,22 @@
 //En este array de strings, tenemos todos los colores
 const colors = ['red', 'green', 'blue', 'yellow', 'pink', 'purple']
 
+const miBoton = document.querySelector(".btn");
+let contador = 0;
 
+// Opción 1
+
+// miBoton.addEventListener("click", event =>{
+//     document.querySelector("body").style.backgroundColor = colors[Math.floor(Math.random() * 6)];
+// });
+
+// Opción 2
+miBoton.addEventListener("click", event =>{
+    document.querySelector("body").style.backgroundColor = colors[contador];
+    if (contador<5){
+        contador++;
+    } else {
+        contador = 0; 
+    }
+    console.log(contador);
+});
